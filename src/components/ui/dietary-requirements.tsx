a680133621e66fc2d474afea6fd26dd9d49752d8
josueJURE <josue.jure@gmail.com>
2025-11-27 10:49:42 +0000

    (JSX): update input element height in DietaryRequirements component

--- src/components/ui/dietary-requirements.tsx
+++ src/components/ui/dietary-requirements.tsx
@@ -8,9 +8,9 @@ function DietaryRequirements() {
       <h1>Indicate any diatery requirements</h1>
       <div className="flex items-center gap-4 pt-4">
         <Label htmlFor="vegan">Vegan</Label>
-        <Input id="vegan" type="checkbox"></Input>
+        <Input className="h-4" id="vegan" type="checkbox"></Input>
         <Label htmlFor="other">Other</Label>
-        <Input id="other" type="checkbox"></Input>
+        <Input className="h-4" id="other" type="checkbox"></Input>
       </div>
     </div>
   );
