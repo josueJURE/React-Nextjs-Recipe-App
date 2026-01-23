1fa2d97378deb5cd20b426176689169e693436a1
josueJURE <josue.jure@gmail.com>
2026-01-23 11:58:19 +0000

    (Prisma/SQL) save recipe to db

--- src/app/api/user/save-recipe-request/route.ts
+++ src/app/api/user/save-recipe-request/route.ts
@@ -1,32 +1,71 @@
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import db from "@/lib/prisma";
+
 import { NextRequest, NextResponse } from "next/server";
-import { recipeContentSchema, RecipeSchema  } from "@/lib/validations/user-choices";
-
-
+import {
+  recipeContentSchema,
+  RecipeSchema,
+} from "@/lib/validations/user-choices";
 
 export async function PATCH(request: NextRequest) {
-    try {
-        const body = await request.json()
-        console.log("body", body)
-        const recipeContentSchemaValidation = recipeContentSchema.safeParse(body)
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
 
-        if(!recipeContentSchemaValidation.success) {
-            return NextResponse.json({
-                error: recipeContentSchemaValidation.error.issues
-            }, {status: 400})
-        }
-
-    const menuContent  = recipeContentSchemaValidation.data
-
-    console.log("menu saved", menuContent)
-
-        return NextResponse.json({
-            message: "recipe added to your favorites"
-
-        }, {status: 200})
-    } catch (error) {
-        return NextResponse.json(
-            { error: "Invalid request" },
-            { status: 500 }
-          );
+    if (!session) {
+      return NextResponse.json(
+        {
+          error: "Unauthorized: User lacks valid authentication credentials",
+        },
+        { status: 401 }
+      );
     }
+
+    const body = await request.json();
+    console.log("body", body);
+    const recipeContentSchemaValidation = recipeContentSchema.safeParse(body);
+
+    if (!recipeContentSchemaValidation.success) {
+      return NextResponse.json(
+        {
+          error: recipeContentSchemaValidation.error.issues,
+        },
+        { status: 400 }
+      );
+    }
+
+    const menuContent = recipeContentSchemaValidation.data;
+
+    const userId = session.user.id
+
+    const savedRecipe = await db.recipe.upsert({
+        where: {
+          userId_content: {
+            userId,
+            content: menuContent
+          }
+        },
+        update: {},
+        create: {
+          content: menuContent,
+          userId
+        }
+      });
+      
+      
+
+    console.log("menu saved", menuContent);
+
+    return NextResponse.json(
+      {
+        message: "recipe added to your favorites",
+        savedRecipe
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid request" }, { status: 500 });
+  }
 }
